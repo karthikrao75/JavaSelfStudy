e266edf5962af3680094023c6be0b738447fc62f
karthikrao75 <karthik.prampalli@gmail.com>
2016-05-15 22:43:19 +0530

    added package cursors in java, enumeration

---
+++ JavaSimpleDemo/src/com/karthik/JavaSimpleDemo/Cursors/EnumerationExample.java
@@ -0,0 +1,28 @@
+package com.karthik.JavaSimpleDemo.Cursors;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class EnumerationExample {
+
+	public static void main(String[] args) {
+		
+		//enumerations(cursors) are used to retrieve objects one by one 
+		
+		Vector<Object> v=new Vector<Object>();
+		v.add("ka");
+		v.add("ke");
+		v.add(1);
+		Enumeration<Object> e=v.elements();
+		
+		while(e.hasMoreElements()){
+			Object o=e.nextElement();
+			System.out.println(o);
+		}
+		
+		// limitations
+		// enumerations can be applied only on legacy classes 
+		// dosen't have any method to remove the object
+	}
+
+}
