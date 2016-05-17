a2b55c7a2428b6db37c600ccaf6c63c0e61fd0b2
karthikrao75 <karthik.prampalli@gmail.com>
2016-05-17 23:47:21 +0530

    TreeSet added

--- JavaSimpleDemo/src/com/infy/JavaSimpleDemo/Collections/TreeSetExample.java
+++ JavaSimpleDemo/src/com/infy/JavaSimpleDemo/Collections/TreeSetExample.java
@@ -8,6 +8,10 @@ public class TreeSetExample {
 	//implements NavigableSet, serializable and cloneable.
 	//internal data structure balanced tree 
 	//no dublicates, insertion order not preserved.
+	//heterogeneous //null only once and as only data in treeset in java 1.6 
+	
+	//if we try to add heterogeneous will give class cast exception
+	//try to add null-null pointer exception  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TreeSet<Object> t=new TreeSet<Object>();
@@ -21,6 +25,7 @@ public class TreeSetExample {
 		//[101, 104, 109, 123, 134]
 		
 		//Sorted set specific methods.
+		
 		System.out.println(t.first());
 		//101
 		System.out.println(t.last());
@@ -31,6 +36,12 @@ public class TreeSetExample {
 		//[109, 123, 134]
 		System.out.println(t.subSet(104, 123));
 		//[104, 109]
+		
+		//constructor that takes object of class that has implemented comparator interface
+		//TreeSet<Object> t1=new TreeSet<>(comparator);
+		
+		//constract takes collection object
+		TreeSet<Object> t2=new TreeSet<>(t);
 	}
 
 }
