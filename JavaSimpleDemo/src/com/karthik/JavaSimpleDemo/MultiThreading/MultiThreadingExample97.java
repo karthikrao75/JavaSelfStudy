0fe6569810e8b3296fb9a69668ca6657b0eb2507
karthikrao75 <karthik.prampalli@gmail.com>
2018-01-27 14:50:26 +0530

    multithreading

---
+++ JavaSimpleDemo/src/com/karthik/JavaSimpleDemo/MultiThreading/MultiThreadingExample97.java
@@ -0,0 +1,13 @@
+package com.karthik.JavaSimpleDemo.MultiThreading;
+
+public class MultiThreadingExample97 {
+
+	/*
+	 * ThreadLocal class provides 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
